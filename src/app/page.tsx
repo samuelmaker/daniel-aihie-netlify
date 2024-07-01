4bccf3bc964a31be0dddda25b516d2867081994a
Suhag Al Amin <developersuhag@gmail.com>
2024-07-01 11:12:42 +0600

    update config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
 import Script from "next/script";
-import { getMarkup } from "@/libs/utils";
 
 export default function Home() {
-  const hero = getMarkup("content/home", "home.md");
-
-  if (!hero) {
-    return null;
-  }
-
-  const { data } = hero;
-
   return (
-    <>
-      {/* Enables registering from this page */}
-      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-      <main>
-        <div className="mt-10 flex flex-col items-center">
-          <h1 className="text-2xl">{data.title}</h1>
-          <p className="text-lg">{data.subtitle}</p>
-          <button>{data.buttonText}</button>
-        </div>
-      </main>
-    </>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
 }
